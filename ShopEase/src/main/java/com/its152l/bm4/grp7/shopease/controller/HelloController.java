a2cfe7eaf71eaae7f65bb3dbe1386a9a6c74a89e
Kayle Matthew Calagui <kmmcalagui@mymail.mapua.edu.ph>
2025-10-21 16:34:25 +0800

    added files

---
+++ ShopEase/src/main/java/com/its152l/bm4/grp7/shopease/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.its152l.bm4.grp7.shopease.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("message", "Hello, ShopEase World!");
+        return "hello"; // Freemarker will look for hello.ftlh
+    }
+}
