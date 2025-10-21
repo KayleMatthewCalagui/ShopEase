b8835173432b5cd52995d757048191d54bafd816
Kayle Matthew Calagui <kmmcalagui@mymail.mapua.edu.ph>
2025-10-21 15:34:26 +0800

    added files

---
+++ ShopEase/src/main/java/com/its152l/bm4/grp7/shopease/ShopEase/ShopEaseApplication.java
@@ -0,0 +1,13 @@
+package com.its152l.bm4.grp7.shopease.ShopEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopEaseApplication.class, args);
+	}
+
+}
---
+++ ShopEase/src/test/java/com/its152l/bm4/grp7/shopease/ShopEase/ShopEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.its152l.bm4.grp7.shopease.ShopEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
